e8e88713db162db3acd014d9624d95f0285793da
Juan Esteban <juanestban000@gmail.com>
2024-11-14 21:17:28 -0500

    feat: added winit, missing test open new window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+#![allow(dead_code, unused_variables, clippy::too_many_arguments, clippy::unnecessary_wraps)]
+use anyhow::Result;
+use winit::application::ApplicationHandler;
+use winit::dpi::LogicalSize;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::window::{Theme, Window, WindowId};
+
+#[derive(Default)]
+struct App {
+  window: Option<Window>,
+}
+
+impl ApplicationHandler for App {
+  fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+    let custom_window = Window::default_attributes()
+      .with_theme(Some(Theme::Dark))
+      .with_title("Sagitario Engine")
+      .with_active(true);
+    self.window = Some(event_loop.create_window(custom_window).unwrap());
+  }
+
+  fn window_event(&mut self, event_loop: &ActiveEventLoop, window_id: WindowId, event: WindowEvent) {
+    match event {
+      WindowEvent::CloseRequested => {
+        print!("The close button was pressed, stopping");
+        event_loop.exit();
+      }
+      WindowEvent::RedrawRequested => {
+        self.window.as_ref().unwrap().request_redraw();
+      }
+      _ => (),
+    }
+  }
+}
+
 fn main() {
-  println!("Hello, world!");
+  println!("Starting window!");
+
+  // win
+
+  let event_loop = EventLoop::new().unwrap();
+  event_loop.set_control_flow(ControlFlow::Poll);
+
+  event_loop.set_control_flow(ControlFlow::Wait);
+
+  let mut app = App::default();
+  event_loop
+    .run_app(&mut app)
+    .expect("Error while running Sagitario Engine");
 }
